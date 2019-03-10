f94004b485a8898f170b619ff0843d69d81606df
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2019-03-10 14:57:41 +0200

    Use babel for modern code support

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-const indexRouter = require('./routes/index');
+import indexRouter from './routes/index';
 
 const app = express();
 
